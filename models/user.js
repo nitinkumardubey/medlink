6940b4bed7da8b96fb8c08d2fdcee3af7361bb76
Nitin Dubey <nitindubey@Nitins-MacBook-Air.local>
2025-10-06 10:00:55 +0530

    updated name

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Hash password before saving
+// Hash password before saving 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
